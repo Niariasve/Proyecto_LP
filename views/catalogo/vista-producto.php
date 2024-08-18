9bf07990dd28dc7e2eea4b587ba677d23d6a4734
jojusuar <jojusuar@espol.edu.ec>
2024-08-17 19:57:00 -0500

    feat!: product page rendering

--- views/catalogo/vista-producto.php
+++ views/catalogo/vista-producto.php
@@ -1,3 +1,8 @@
-<?php echo "hola mijas";?>
-
-<h1>ID del producto:  <?php echo $id; ?> </h1>
+<h1><?php echo $producto->titulo; ?> </h1>
+<a href='/catalogo'>Volver a la tienda</a>
+<img src=<?php echo $producto->imagen; ?> width=500 height=500>
+<h2>Precio: <?php echo $producto->precio; ?> </h2>
+<h2>Descripcion: <?php echo $producto->descripcion; ?> </h2>
+<h2>Estado: <?php echo $producto->estado; ?> </h2>
+<h2>Condicion: <?php echo $producto->condicion; ?> </h2>
+<h2>Vendedor: <?php echo $producto->usuario; ?> </h2>
