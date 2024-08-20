f5795230273ae9d634daa56ac8fffca1ffda43a7
jojusuar <jojusuar@espol.edu.ec>
2024-08-19 23:53:23 -0500

    fix: php echo converted to html with embedded php

--- views/catalogo/editor.php
+++ views/catalogo/editor.php
@@ -1,23 +1,22 @@
-<?php
-echo "<h1>Editar $producto->titulo</h1>";
+<h1>Editar <?php echo htmlspecialchars($producto->titulo); ?></h1>
 
-echo '<form action="/guardar-cambios" method="post">
+<form action="/guardar-cambios" method="post">
     <div class="campo">
         <label for="ftitulo">Titulo de la publicacion</label>
         <input 
         id="ftitulo"
         name="titulo"
-        value="' . htmlspecialchars($producto->titulo) . '"
+        value="<?php echo htmlspecialchars($producto->titulo); ?>"
         required
         >
     </div>
     <div class="campo">
-        <label for="fprecio">precio</label>
+        <label for="fprecio">Precio</label>
         <input
         type="number"
         id="fprecio"
         name="precio"
-        value="' . htmlspecialchars($producto->precio) . '"
+        value="<?php echo htmlspecialchars($producto->precio); ?>"
         step="0.01"
         required
         >
@@ -27,7 +26,7 @@ echo '<form action="/guardar-cambios" method="post">
         <input
         id="fdescripcion"
         name="descripcion"
-        value="' . htmlspecialchars($producto->descripcion) . '"
+        value="<?php echo htmlspecialchars($producto->descripcion); ?>"
         required
         >
     </div>
@@ -36,7 +35,7 @@ echo '<form action="/guardar-cambios" method="post">
         <input
         id="festado"
         name="estado"
-        value="' . htmlspecialchars($producto->estado) . '"
+        value="<?php echo htmlspecialchars($producto->estado); ?>"
         required
         >
     </div>
@@ -45,21 +44,20 @@ echo '<form action="/guardar-cambios" method="post">
         <input
         id="fcondicion"
         name="condicion"
-        value="' . htmlspecialchars($producto->condicion) . '"
+        value="<?php echo htmlspecialchars($producto->condicion); ?>"
         required
         >
     </div>
     <div class="campo">
-        <label for="fimagen">imagen</label>
+        <label for="fimagen">Imagen</label>
         <input
         id="fimagen"
         name="imagen"
-        value="' . htmlspecialchars($producto->imagen) . '"
+        value="<?php echo htmlspecialchars($producto->imagen); ?>"
         >
     </div>
-    <input type=hidden name="antiguo" value=" '. urlencode(json_encode($producto)) . '">
+    <input type="hidden" name="antiguo" value="<?php echo urlencode(json_encode($producto)); ?>">
     <input type="submit" value="Actualizar producto">
-</form>';
+</form>
 
-echo "<a href='/vista-producto/?id=".md5($producto->usuario.$producto->titulo)."'>Cancelar</a>";
-?>
+<a href="/vista-producto/?id=<?php echo md5($producto->usuario . $producto->titulo); ?>">Cancelar</a>
